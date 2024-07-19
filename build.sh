6832f3db6c92f3d8e674665864a2fb3d9cf3706d
cashhollister2u <153677541+cashhollister2u@users.noreply.github.com>
2024-07-19 12:48:43 -0400

    consolidate build.sh and refresh.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+
+# Function to handle hot refresh
+refresh() {
+    # make the .war dir
+    mvn clean package -P exploded
+
+    # move the updated dir to tomcat
+    sudo cp -r  target/java_webapp /usr/local/tomcat9/webapps/
+
+    # restart tomcat server
+    /usr/local/tomcat9/bin/shutdown.sh
+    /usr/local/tomcat9/bin/startup.sh
+
+    # Get the directory where the script is located
+    dir_path=$(dirname "$(realpath "$0")")
+
+    # Extract the directory name
+    dir_name=$(basename "$dir_path")
+
+    echo ""
+    echo ""
+    echo "Build Complete"
+    echo ""
+    echo "web page running on http://localhost:8080/$dir_name"
+}
+
+#make refresh function available to being called by entr when file is saved
+export -f refresh
+
 # Function to handle shutdown
 shutdown() {
     echo "Shutting down Tomcat..."
@@ -14,8 +43,11 @@ echo "Building java webApp"
 echo ""
 echo ""
 
-/usr/local/tomcat9/bin/startup.sh
+# start server before monitoring 
+/usr/local/tomcat9/bin/startup.sh 
 
-find src -type f | entr -r ./refresh.sh
+# when change occures in "src" dir run the refresh function above
+find src -type f | entr -r bash -c 'refresh'
 
+#close server when script ends
 shutdown
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2 style="font-size: 12px;">compiler test 3</h2>
+<h2 style="font-size: 12px;">compiler test 5
+</h2>
 </body>
 </html>
